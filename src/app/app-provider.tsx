ac50e8ce2b22a8deca61cd85c3a7aa1eca9d0ceb
Grigory <grinmorg@gmail.com>
2023-12-14 22:48:02 +0300

    remove appwrite methods

--- src/app/app-provider.tsx
+++ src/app/app-provider.tsx
@@ -1,12 +1,10 @@
-import { queryClient } from "@/shared/api/query-client";
 import { AppWrapper } from "@/shared/context";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
 
 export function AppProvider({ children }: { children?: ReactNode }) {
   return (
     <AppWrapper>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      {children}
     </AppWrapper>
   );
 }
